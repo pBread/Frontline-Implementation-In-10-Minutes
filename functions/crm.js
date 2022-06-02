55b1a7074f9eeb735085ab16f40c826607f8f08e
Phil <pbrede21@gmail.com>
2022-06-02 10:53:41 -0600

    updated CRM

--- functions/crm.js
+++ functions/crm.js
@@ -1,3 +1,18 @@
-exports.handler = function (context, event, callback) {
-  return callback(null, { yoko: "ono" });
+const jsforce = require("jsforce");
+const sfdc = new jsforce.Connection({});
+
+exports.handler = async function (context, event, callback) {
+  await sfdc.login(process.env.SF_USER, process.env.SF_PASSWORD);
+
+  switch (event.Location) {
+    case "GetCustomersList":
+      return getCustomersList(context, event, callback);
+
+    case "GetCustomerDetailsByCustomerId":
+      return getCustomerDetailsByCustomerId(context, event, callback);
+  }
 };
+
+async function getCustomersList(context, event, callback) {}
+
+async function getCustomerDetailsByCustomerId(context, event, callback) {}
