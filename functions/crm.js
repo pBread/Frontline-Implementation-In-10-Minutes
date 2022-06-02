ae201fa644f8cb691488dff8d858231ce86ba824
Phil <pbrede21@gmail.com>
2022-06-02 11:23:28 -0600

    starting from scratch

--- functions/crm.js
+++ functions/crm.js
@@ -1,18 +1,3 @@
-const jsforce = require("jsforce");
-const sfdc = new jsforce.Connection({});
+// github.com/pBread/Frontline-Implementation-In-10-Minutes
 
-exports.handler = async function (context, event, callback) {
-  await sfdc.login(process.env.SF_USER, process.env.SF_PASSWORD);
-
-  switch (event.Location) {
-    case "GetCustomersList":
-      return getCustomersList(context, event, callback);
-
-    case "GetCustomerDetailsByCustomerId":
-      return getCustomerDetailsByCustomerId(context, event, callback);
-  }
-};
-
-async function getCustomersList(context, event, callback) {}
-
-async function getCustomerDetailsByCustomerId(context, event, callback) {}
+exports.handler = async function (context, event, callback) {};
